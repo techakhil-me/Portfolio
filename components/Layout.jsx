0459c1c5f2cb90403b908da44510a924f546f6c3
TechAkhil <techakhilc47@gmail.com>
2021-11-21 14:28:48 +0530

    scroll fixed

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,19 +9,6 @@ import {
 import { AnimateSharedLayout } from "framer-motion";
 import { useRouter } from "next/router";
 
-const ResetScroll = (props) => {
-  const { pathname } = useRouter();
-  const { scroll } = useLocomotiveScroll();
-
-  useEffect(() => {
-    scroll &&
-      scroll.scrollTo("top", {
-        duration: 0,
-        disableLerp: true
-      });
-  }, [pathname, scroll]);
-  return <>{props.children}</>;
-};
 
 const Layout = (props) => {
   const containerRef = useRef(null);
@@ -42,18 +29,15 @@ const Layout = (props) => {
           scroll.scrollTo(0, { duration: 0, disableLerp: true })
         } // If you want to reset the scroll position to 0 for example
         onUpdate={() => console.log("Updated, but not on location change!")}
-      >
-        <ResetScroll>
+      ><div data-scroll-container className="w-screen" ref={containerRef}>
           <div
-            data-scroll-container
-            ref={containerRef}
             className="Layout md:px-4 container mx-auto bg-dark antialiased text-light w-screen h-full"
           >
             <Navbar />
             {props.children}
             <Footer scroll={scroll}/>
           </div>
-        </ResetScroll>
+          </div>
       </LocomotiveScrollProvider>
 
       <Cursor />
